09e4264fa71cdfa38a5e04b372946aa8aed2dd75
Chrysanthe Belgique <belgiquc@byu.edu>
2024-01-31 10:03:15 -0700

    add supporting instance

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿// Description: TicTacToe game, taking user input to play the game
 // Authors: Kian Bangerter, Chrysanthe Belgique, Lorin Costley, Ryan Hafen
 
+// Supporting s = new Supporting();
+
 object[] board = [1,2,3,4,5,6,7,8,9];
 
 Console.WriteLine("Welcome to TicTacToe!");
